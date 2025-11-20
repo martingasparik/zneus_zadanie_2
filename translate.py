ad3e51cbe0d3966494aa44002b4f391a6a8ee583
simonmizerak <145459594+SimonMizerak@users.noreply.github.com>
2025-11-20 15:24:26 +0100

    EDA + fixed translate
    Added basic EDA and fixed a bug in the translate dictionary

---
+++ translate.py
@@ -0,0 +1,12 @@
+translate = {
+    "cane": "dog",
+    "cavallo": "horse",
+    "elefante": "elephant",
+    "farfalla": "Butterfly",
+    "gallina": "hen",
+    "gatto": "cat",
+    "mucca": "cow",
+    "pecora": "sheep",
+    "ragno": "spider",
+    "scoiattolo": "squirrel"
+}
